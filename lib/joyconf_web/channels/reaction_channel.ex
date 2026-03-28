1ce86fc88761e3ce4143041def2ac5735a9a732b
Tracy Atteberry <tracy@magicbydesign.com>
2026-03-27 19:25:11 -0500

    feat: add session start/stop to ReactionChannel

--- lib/joyconf_web/channels/reaction_channel.ex
+++ lib/joyconf_web/channels/reaction_channel.ex
@@ -12,4 +12,33 @@ defmodule JoyconfWeb.ReactionChannel do
         {:ok, assign(socket, :talk, talk)}
     end
   end
+
+  def handle_in("start_session", _payload, socket) do
+    case Talks.start_session(socket.assigns.talk) do
+      {:ok, session} ->
+        {:reply, {:ok, %{session_id: session.id, label: session.label}}, socket}
+
+      {:error, _changeset} ->
+        {:reply, {:error, %{reason: "failed"}}, socket}
+    end
+  end
+
+  # Guard against double-stop: if ended_at is already set, reply ok without
+  # overwriting the original end timestamp.
+  def handle_in("stop_session", %{"session_id" => session_id}, socket) do
+    case Talks.get_session(session_id) do
+      nil ->
+        {:reply, {:error, %{reason: "not_found"}}, socket}
+
+      %{ended_at: ended_at} when not is_nil(ended_at) ->
+        {:reply, :ok, socket}
+
+      %{talk_id: talk_id} = session when talk_id == socket.assigns.talk.id ->
+        {:ok, _} = Talks.stop_session(session)
+        {:reply, :ok, socket}
+
+      _session ->
+        {:reply, {:error, %{reason: "unauthorized"}}, socket}
+    end
+  end
 end
