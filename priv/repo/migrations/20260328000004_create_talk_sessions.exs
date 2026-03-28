ad38701206ac2cd0af46c3ec37d50666ea2a2d4f
Tracy Atteberry <tracy@magicbydesign.com>
2026-03-27 19:00:11 -0500

    feat: add talk_sessions migration

---
+++ priv/repo/migrations/20260328000004_create_talk_sessions.exs
@@ -0,0 +1,16 @@
+defmodule Joyconf.Repo.Migrations.CreateTalkSessions do
+  use Ecto.Migration
+
+  def change do
+    create table(:talk_sessions) do
+      add :talk_id, references(:talks, on_delete: :delete_all), null: false
+      add :label, :string, null: false
+      add :started_at, :utc_datetime, null: false
+      add :ended_at, :utc_datetime
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:talk_sessions, [:talk_id])
+  end
+end
