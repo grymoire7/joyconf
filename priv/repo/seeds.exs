aeae5c5e73a0567bdb57761d1a0980bae0f43c1a
Tracy Atteberry <tracy@magicbydesign.com>
2026-04-12 22:41:30 -0500

    feat: update seeds to create initial admin user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,19 @@
-# Script for populating the database. You can run it as:
+# priv/repo/seeds.exs
 #
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Speechwave.Repo.insert!(%Speechwave.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+# Run with: mix run priv/repo/seeds.exs
+# Idempotent: safe to run multiple times.
+
+alias Speechwave.{Accounts, Repo}
+
+admin_email = System.get_env("ADMIN_EMAIL") || "admin@speechwave.live"
+admin_password = System.fetch_env!("ADMIN_SEED_PASSWORD")
+
+case Accounts.get_user_by_email(admin_email) do
+  nil ->
+    {:ok, user} = Accounts.register_user(%{email: admin_email, password: admin_password})
+    Repo.update!(Ecto.Changeset.change(user, is_admin: true))
+    IO.puts("Admin user created: #{admin_email}")
+
+  existing ->
+    IO.puts("Admin user already exists: #{existing.email}")
+end
